230038e05d58ccd033d1591fe0ee114418474e3c
ysagar11y <ysagar11y@gmail.com>
2024-09-27 12:21:20 +0530

    First Commit

---
+++ src/test/java/org/stayeazy/StayEazyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stayeazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayEazyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
