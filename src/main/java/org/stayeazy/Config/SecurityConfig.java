beda52c09d68b01224c9bf6a06f08369caedd85d
ysagar11y <ysagar11y@gmail.com>
2024-10-21 22:55:26 +0530

    First Commit

---
+++ src/main/java/org/stayeazy/Config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package org.stayeazy.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.csrf(csrf->csrf.disable())
+                .authorizeRequests()
+                .requestMatchers("/register").permitAll()
+                .requestMatchers("/login").authenticated()
+                .and()
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
