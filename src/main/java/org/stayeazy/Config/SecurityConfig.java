427eb8f66dc48f0ce4e84e878968bed3fe3445f0
ysagar11y <ysagar11y@gmail.com>
2024-10-21 22:58:08 +0530

    Security Changes first commit

--- src/main/java/org/stayeazy/Config/SecurityConfig.java
+++ src/main/java/org/stayeazy/Config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +20,9 @@ public class SecurityConfig {
                 .and()
                 .formLogin(Customizer.withDefaults())
                 .build();
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
